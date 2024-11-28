5e0dba15b901163b4b1e2abdf0e4c811225dba9f
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-11-29 01:13:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}

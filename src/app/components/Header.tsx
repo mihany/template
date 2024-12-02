1e59d1f61ad697afeb107586044616fb84763f4b
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-12-02 20:06:20 +0200

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+
 import { useState } from "react";
 import {
   Dialog,
@@ -70,10 +72,10 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="./" className="-m-1.5 p-1.5">
+          <Link href="./" className="-m-1.5 p-1.5">
             <span className="sr-only">Hoetzin</span>
             <Image
-              className="dark:invert"
+              // className="dark:invert"
               src="../logo.svg"
               alt="Next.js logo"
               width={100}
@@ -85,7 +87,7 @@ export default function Header() {
               src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
               className="h-8 w-auto"
             /> */}
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button

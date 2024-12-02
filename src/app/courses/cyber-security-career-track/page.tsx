9d6fc79c5528372910323469f4ed1436bf44d223
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-12-02 14:51:44 +0200

    image added in courses

--- src/app/courses/cyber-security-career-track/page.tsx
+++ src/app/courses/cyber-security-career-track/page.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 export default function CyberSecurityCareerTrack() {
   return (
     <div>
       <h1>Cyber Security Track Course Page</h1>
       <p>Welcome to Cyber Security Track</p>
+      <Image
+        className="dark:invert"
+        src="../next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
     </div>
   );
 }

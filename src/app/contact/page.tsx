4619b53a09a47799d3b9c0bf2f3d482495963b6e
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-12-03 23:27:17 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "Contact Us - Hoetzin",
   description:
@@ -74,11 +76,26 @@ export default function Contact() {
             <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
               Get in touch
             </h2>
-            <p className="mt-6 text-lg/8 text-gray-600">
+            <div className="flex items-center gap-x-6 mt-6">
+              <Image
+                alt=""
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="size-16 rounded-full"
+              />
+              <div>
+                <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">
+                  Adam Smith
+                </h3>
+                <p className="text-sm/6 font-semibold text-indigo-600">
+                  Student Advisor
+                </p>
+              </div>
+            </div>
+            {/* <p className="mt-6 text-lg/8 text-gray-600">
               Proin volutpat consequat porttitor cras nullam gravida at. Orci
               molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
               Arcu sed malesuada et magna.
-            </p>
+            </p> */}
             <dl className="mt-10 space-y-4 text-base/7 text-gray-600">
               <div className="flex gap-x-4">
                 <dt className="flex-none">
